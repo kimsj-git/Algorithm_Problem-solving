a6860ad94a2e002d0655fe6e36edcc48148aa611
Suhjeong Kim <109324498+kimsj-git@users.noreply.github.com>
2022-08-02 23:34:01 +0900

    [Silver IV] Title: 큐, Time: 80 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.py
@@ -0,0 +1,32 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+q = []
+for _ in range(N):
+    cmd = list(input().split())
+    if cmd[0] == 'push':
+        q.append(int(cmd[1]))
+    elif cmd[0] == 'pop':
+        if q:
+            pop = q.pop(0)
+            print(pop)
+        else:
+            print(-1)
+    elif cmd[0] == 'size':
+        print(len(q))
+    elif cmd[0] == 'empty':
+        if q:
+            print(0)
+        else:
+            print(1)
+    elif cmd[0] == 'front':
+        if q:
+            print(q[0])
+        else:
+            print(-1)
+    elif cmd[0] == 'back':
+        if q:
+            print(q[-1])
+        else:
+            print(-1)
