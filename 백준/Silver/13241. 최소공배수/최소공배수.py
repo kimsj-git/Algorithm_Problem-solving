699d745b385787ddc49e843a722e69e6c2b47bfc
Suhjeong Kim <109324498+kimsj-git@users.noreply.github.com>
2022-08-07 14:30:13 +0900

    [Silver V] Title: 최소공배수, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/13241. 최소공배수/최소공배수.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+def gcd(x, y):
+    while y:
+        x, y = y, x % y
+    return x
+
+def lcm(x, y):
+    return int(x*y / gcd(x, y))
+
+A, B = map(int, input().split())
+print(lcm(A, B))
