4143af510bdd96eafafb760c3ec091e70de8eb71
Suhjeong Kim <109324498+kimsj-git@users.noreply.github.com>
2022-09-30 15:01:51 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 448 ms, Memory: 63704 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.py
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.py
@@ -1,12 +1,13 @@
-from heapq import *
+from heapq import heappush, heappop
+import sys
+input = sys.stdin.readline
 INF = float('inf')
 
 
-def dijkstra(start, end):
+def dijkstra(start, *end):
     distance = {i: INF for i in range(1, N+1)}
     distance[start] = 0     # distance[i]: start부터 i 정점까지의 최단거리
     pq = []
-    heapify(pq)
     heappush(pq, (0, start))
 
     while pq:
@@ -19,7 +20,7 @@ def dijkstra(start, end):
                 distance[nu] = updated_dist
                 heappush(pq, (updated_dist, nu))
     
-    return distance[end]
+    return tuple([distance[i] for i in end])
 
 
 N, E = map(int, input().split())
@@ -31,10 +32,19 @@ for _ in range(E):
 
 v1, v2 = map(int, input().split())
 
-# 1. 경로 순서 1 -> v1 -> v2 -> N 의 최단경로
-result1 = dijkstra(1, v1) + dijkstra(v1, v2) + dijkstra(v2, N)
-# 2. 경로 순서 1 -> v2 -> v1 -> N 의 최단경로
-result2 = dijkstra(1, v2) + dijkstra(v2, v1) + dijkstra(v1, N)
+# # 1. 경로 순서 1 -> v1 -> v2 -> N 의 최단경로
+# result1 = dijkstra(1, v1) + dijkstra(v1, v2) + dijkstra(v2, N)
+# # 2. 경로 순서 1 -> v2 -> v1 -> N 의 최단경로
+# result2 = dijkstra(1, v2) + dijkstra(v2, v1) + dijkstra(v1, N)
+
+# v1 -> 1, v1 -> v2, v1 -> N 을 구하기
+d_v1_1, d_v1_v2, d_v1_N = dijkstra(v1, 1, v2, N)
+
+# v2 -> 1, v2 -> N 을 구하기
+d_v2_1, d_v2_N = dijkstra(v2, 1, N)
+
+result1 = d_v1_1 + d_v1_v2 + d_v2_N
+result2 = d_v2_1 + d_v1_v2 + d_v1_N
 
 ans = min(result1, result2)
 
