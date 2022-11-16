c4087a0446cefd680e23ca4979cc35045165e8be
Suhjeong Kim <109324498+kimsj-git@users.noreply.github.com>
2022-11-16 12:29:43 +0900

    [Bronze I] Title: 팰린드롬수, Time: 76 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
@@ -1,18 +1,8 @@
 while True:
-    num = input()
-    if num == '0':
+    a = input()
+    if a == '0':
         break
-    
-    n = len(num)
-    is_palindrome = True
-    for i in range(n//2):
-        if num[i] == num[n-i-1]:
-            continue
-        else:
-            is_palindrome = False
-            break
-    
-    if is_palindrome:
+    if a == a[::-1]:
         print('yes')
     else:
         print('no')
