186782e65d445cd3f916e8a1c5e7cfffe4765966
Suhjeong Kim <109324498+kimsj-git@users.noreply.github.com>
2022-11-16 15:15:47 +0900

    [Silver IV] Title: 수 찾기, Time: 196 ms, Memory: 49768 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -0,0 +1,9 @@
+n = int(input())
+a = set(list(map(int,input().split())))
+m = int(input())
+b = list(map(int,input().split()))
+for i in b:
+    if i in a:
+        print(1)
+    else:
+        print(0)
