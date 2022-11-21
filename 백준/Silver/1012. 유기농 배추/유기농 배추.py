e537ddf2a89b57dfe43acca8d0347053079f944f
Suhjeong Kim <109324498+kimsj-git@users.noreply.github.com>
2022-11-21 16:38:14 +0900

    [Silver II] Title: 유기농 배추, Time: 88 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,34 +1,41 @@
-T = int(input())
-for tc in range(1, T+1):
-    M, N, K = map(int, input().split())
-    
-    # N: 행, M: 열
-    arr = [[0] * M for _ in range(N)]
-    for _ in range(K):
-        j, i = map(int, input().split())
-        arr[i][j] = 1
+import sys
 
-    count = 0
-    for i in range(N):
-        for j in range(M):
-            # 1이면, 인접한 배추 다 뽑기
-            if arr[i][j] == 1:
-                count += 1      # 지렁이 추가
-                
-                # BFS 이용, 인접 배추 다 뽑기
-                v = (i, j)
-                queue = []
-                queue.append(v)
-                arr[v[0]][v[1]] = 0   # 배추뽑
-                while queue:
-                    t = queue.pop(0)    # 현재 위치 t
-                    # 인접 배추 좌표 w
-                    dij = [[-1, 0], [1, 0], [0, -1], [0, 1]]
-                    for di, dj in dij:
-                        w = (t[0] + di, t[1] + dj)
-                        if w[0] < 0 or w[0] >= N or w[1] < 0 or w[1] >= M:
-                            continue
-                        if arr[w[0]][w[1]] == 1:
-                            queue.append(w)
-                            arr[w[0]][w[1]] = 0    # 배추뽑
-    print(count)
+T = int(sys.stdin.readline())
+
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
+
+def BFS(x,y):
+    queue = [(x,y)]
+    matrix[x][y] = 0
+
+    while queue:
+        x,y = queue.pop(0)
+
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if nx < 0 or nx >= M or ny < 0 or ny >= N:
+                continue
+
+            if matrix[nx][ny] == 1:
+                queue.append((nx, ny))
+                matrix[nx][ny] = 0
+
+for tc in range(T):
+    M,N,K = map(int,sys.stdin.readline().split())
+    matrix = [[0] * (N) for _ in range(M)]
+    cnt = 0
+
+    for j in range(K):
+        x,y = map(int,sys.stdin.readline().split())
+        matrix[x][y] = 1
+
+    for a in range(M):
+        for b in range(N):
+            if matrix[a][b] == 1:
+                BFS(a,b)
+                cnt += 1
+
+    print(cnt)
