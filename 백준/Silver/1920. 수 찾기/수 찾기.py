de0679e29058e54e9eeb27a9443ec25a150de035
Suhjeong Kim <109324498+kimsj-git@users.noreply.github.com>
2022-12-20 22:12:17 +0900

    [Silver IV] Title: 수 찾기, Time: 180 ms, Memory: 49420 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,9 +1,11 @@
-n = int(input())
-a = set(list(map(int,input().split())))
-m = int(input())
-b = list(map(int,input().split()))
-for i in b:
-    if i in a:
+N = int(input())
+A = set(map(int, input().split()))
+M = int(input())
+nums = list(map(int, input().split()))
+
+# average time complexity for set is O(n)
+for num in nums:
+    if num in A:
         print(1)
     else:
         print(0)
