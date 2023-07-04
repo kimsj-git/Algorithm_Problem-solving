e24a36d20534a74057b36b81e69867652e47e189
Suhjeong Kim <kimsj.py@gmail.com>
2023-07-04 15:58:47 +0900

    [Gold III] Title: 아기 상어, Time: 240 ms, Memory: 34272 KB -BaekjoonHub

--- 백준/Gold/16236. 아기 상어/아기 상어.py
+++ 백준/Gold/16236. 아기 상어/아기 상어.py
@@ -1,23 +1,25 @@
+from collections import deque
+
 N = int(input())
 fish = [list(map(int, input().split())) for _ in range(N)]
 
 
 # 다음에 먹을 물고기의 좌표(w)와 거리(nearest)를 반환하는 함수
 # 조건: 1)최소거리, 2)위, 왼쪽 순서로 우선
-def nearest_fish(arr, n, start, shark_size):
-    queue = [start]
-    distance = [[None] * n for _ in range(n)]   # 거리 함수
+def nearest_fish_bfs(arr, n, start, shark_size):
+    queue = deque([start])
+    distance = [[None] * n for _ in range(n)]  # 거리 함수
     distance[start[0]][start[1]] = 0
-    nearest = 0     # 가장 가까운 거리
-    edible = []     # 먹을 수 있는 물고기 좌표 저장
+    nearest = 0  # 가장 가까운 거리
+    edible = []  # 먹을 수 있는 물고기 좌표 저장
 
     # BFS 탐색
     while queue:
-        v = queue.pop(0)
-        dij = [[-1, 0], [0, -1], [0, 1], [1, 0]] # 상좌우하 델타이동
+        v = queue.popleft()
+        dij = [[-1, 0], [0, -1], [0, 1], [1, 0]]  # 상좌우하 델타이동
         for di, dj in dij:
             w = [v[0] + di, v[1] + dj]  # 주변 좌표
-            if (0 <= w[0] < N and 0 <= w[1] < N and distance[w[0]][w[1]] == None):
+            if 0 <= w[0] < N and 0 <= w[1] < N and distance[w[0]][w[1]] == None:
                 # 물고기 없으면: 지나감
                 if arr[w[0]][w[1]] == 0:
                     queue.append(w)
@@ -33,12 +35,12 @@ def nearest_fish(arr, n, start, shark_size):
                 elif 0 < arr[w[0]][w[1]] < shark_size:
                     queue.append(w)
                     distance[w[0]][w[1]] = distance[v[0]][v[1]] + 1
-                    
+
                     edible.append(w)
                     # nearest에 처음으로 저장되는 값이 최소거리(BFS 탐색)
                     if nearest == 0:
                         nearest = distance[w[0]][w[1]]
-    
+
     # <중요> edible에 포함된 좌표를 정렬해줘야, 순서 조건을 만족할 수 있음
     edible.sort()
     for fish in edible:
@@ -48,18 +50,16 @@ def nearest_fish(arr, n, start, shark_size):
 
 
 # 먹을 수 있는 물고기 다 찾기까지 걸리는 시간을 반환하는 함수
-def sorted_BFS_shark(arr, n, shark):
+def get_eating_time(arr, n, shark):
     result = 0  # 총 시간
-    size = 2    # 아기상어 크기
-    ate = 0     # 먹은 개수
+    size = 2  # 아기상어 크기
+    ate = 0  # 먹은 개수
     arr[shark[0]][shark[1]] = 0
-    
-    queue = [shark]
-    while queue:
-        v = queue.pop(0)    # 현재 상어 좌표
-        w, time = nearest_fish(arr, n, v, size)   # 다음 먹을 물고기 위치, 걸리는 시간
-        
-        # 먹을 물고기가 물고기가 없다면, 결과 반환
+
+    while True:
+        w, time = nearest_fish_bfs(arr, n, shark, size)  # 다음 먹을 물고기 위치, 걸리는 시간
+
+        # 먹을 물고기가 없다면, 결과 반환
         if not w:
             return result
 
@@ -68,12 +68,11 @@ def sorted_BFS_shark(arr, n, shark):
         if ate == size:
             size += 1
             ate = 0
-        
+
         # 먹은 후: 시간 체크, 먹은 위치부터 다시 while문 시작
         result += time  # 총 시간 체크
-        arr[w[0]][w[1]] = 0         # 먹은 물고기 제거
-        queue = [w]                 # 큐 초기화
-    return result
+        arr[w[0]][w[1]] = 0  # 먹은 물고기 제거
+        shark = w  # 상어를 변경된 위치로 이동
 
 
 # 아기상어 최초 위치 찾기
@@ -87,4 +86,4 @@ for i in range(N):
     if is_shark:
         break
 
-print(sorted_BFS_shark(fish, N, shark))
+print(get_eating_time(fish, N, shark))
