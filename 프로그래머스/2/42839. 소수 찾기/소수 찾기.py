92c7b43edf2da6c8914e9b025fa6962662e3b960
Suhjeong Kim <kimsj.py@gmail.com>
2024-03-13 21:39:59 +0900

    [level 2] Title: 소수 찾기, Time: 0.34 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -0,0 +1,25 @@
+from itertools import permutations
+
+
+def is_prime(x):
+    if x == 0 or x == 1:
+        return False
+    for div in range(2, int(x ** 0.5) + 1):
+        if x % div == 0:
+            return False
+    return True
+
+
+def solution(numbers):
+    n = len(numbers)
+    prime_numbers = set([])
+    for i in range(1, 1 << n):
+        subset = []
+        for j in range(n):
+            if i & (1 << j):
+                subset.append(numbers[j])
+        for permu in permutations(subset, len(subset)):
+            num = int(''.join(permu))
+            if is_prime(num):
+                prime_numbers.add(num)
+    return len(prime_numbers)
