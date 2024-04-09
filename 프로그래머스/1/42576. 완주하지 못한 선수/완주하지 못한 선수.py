f0ab834322db69243ec241fe7069fe6bd6daa4f4
Suhjeong Kim <kimsj.py@gmail.com>
2024-04-09 21:33:40 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 57.74 ms, Memory: 34 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -0,0 +1,12 @@
+from collections import defaultdict
+
+
+def solution(participant, completion):
+    participant_dict = defaultdict(lambda: 0)
+    for p in participant:
+        participant_dict[p] += 1
+    for c in completion:
+        participant_dict[c] -= 1
+    for key, value in participant_dict.items():
+        if value > 0:
+            return key
