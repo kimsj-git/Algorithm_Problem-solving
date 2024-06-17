483238916522077a4b96dadd4c93a2528c808697
Suhjeong Kim <kimsj.py@gmail.com>
2024-06-17 14:39:15 +0900

    [level 0] Title: 문자열의 앞의 n글자, Time: 9.23 ms, Memory: 77.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181907. 문자열의 앞의 n글자/문자열의 앞의 n글자.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(String my_string, int n) {
+        String answer = "";
+        
+        for (int i = 0; i < n; i++) {
+            answer += my_string.charAt(i);
+        }
+        return answer;
+    }
+}
